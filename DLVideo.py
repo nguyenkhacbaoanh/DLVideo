beb85b1b025194a53af1bc1c0bcaf5a53a9e10d2
bao anh <khacbaoanh.nguyen@gmail.com>
2017-07-15 11:55:53 +0200

    resolve save's location

--- DLVideo.py
+++ DLVideo.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.filedialog import * #to open and find files
 import pafy
+import os
 
 class App:
     def __init__(self):
@@ -19,14 +20,14 @@ class App:
 
         self.filemenu = Menu(self.menu)
         self.menu.add_cascade(label="File", menu=self.filemenu)
-        self.filemenu.add_command(label="New", command=self.find_direction)
-        self.filemenu.add_command(label="Open...", command=self.find_direction)
+        self.filemenu.add_command(label="New")
+        self.filemenu.add_command(label="Open...")
         self.filemenu.add_separator()
         self.filemenu.add_command(label="Exit", command=exit)
 
         self.helpmenu = Menu(self.menu)
         self.menu.add_cascade(label="Help", menu=self.helpmenu)
-        self.helpmenu.add_command(label="About...", command=self.find_direction)
+        self.helpmenu.add_command(label="About...")
         
         # button
         self.b1 = Button(self.root, text="Download", command=self.download)
@@ -55,32 +56,34 @@ class App:
         self.save_local = Entry(self.root)
         self.save_local.grid(row=2,column=1,padx=15,pady=15,ipadx=50,ipady=10,sticky="e")
         
-        
 
 
     # save file:
     # Open filedialog and return adress of save's location
     def find_direction(self):
-        location_file_save = asksaveasfilename(title="Save your video",parent=self.root,filetypes=[('video files','.mp4'),('webm','.webm'),('all files','.*')])
-        return str(location_file_save)
+        name_file = str(pafy.new(self.url.get()).title)
+        location_file_save = asksaveasfilename(title="Save your video",parent=self.root,initialfile=name_file,filetypes=[('video files','.mp4'),('webm','.webm'),('all files','.*')])
+        return location_file_save
     
     # get value save's location in Entry
     def get_value_from_button(self):
         self.adr = self.find_direction()
         self.save_local.delete(0, END)
-        self.save_local.insert(0, self.adr)
+        self.save_local.insert(0, str(self.adr))
 
     def download(self):
         video = pafy.new(self.url.get())
+        # video.title 
         if self.check_quality['indicatoron'] == 1: #checkbox: whether best_resolution have clicked or not
             best = video.getbest()
         else:
             best = video.getbest(preftype="webm")
-        return best.download(quiet=False)
+        return best.download(filepath=os.path.dirname(self.save_local.get()),quiet=False)
     
 
     def run_code(self):
         mainloop()
+        
 
 
 
